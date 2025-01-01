b85092361e68d596ff2042761a9894dc0eb8deb2
Vladislav <neo_120@mail.ru>
2025-01-02 02:19:33 +0300

    rebuild.sh script fixed

--- deploy/scripts/rebuild.sh
+++ deploy/scripts/rebuild.sh
@@ -2,4 +2,6 @@
 
 cd "$(dirname "$0")/.." || exit 1
 
+docker compose rm --stop --force --volumes
+docker compose down --volumes
 docker compose --env-file .env --env-file .env.local create --force-recreate --build

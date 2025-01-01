941e9b3e8055433de3a352bba09781c7a23eb18b
Vladislav <neo_120@mail.ru>
2025-01-01 20:16:47 +0300

    update.sh script updated

--- deploy/scripts/update.sh
+++ deploy/scripts/update.sh
@@ -10,7 +10,7 @@ test -e ./passmeta-ui-tmp && rm -r ./passmeta-ui-tmp
 test -e ./passmeta-ui-new && rm -r ./passmeta-ui-new
 
 # get and unzip
-wget -P ./passmeta-ui-tmp https://github.com/vlad120/PassMeta-WebApp/archive/refs/heads/master.zip || exit 1
+wget -P ./passmeta-ui-tmp https://github.com/defhid/PassMeta-WebApp/archive/refs/heads/master.zip || exit 1
 unzip ./passmeta-ui-tmp/master.zip -d ./passmeta-ui-tmp || exit 1
 
 # prepare files
